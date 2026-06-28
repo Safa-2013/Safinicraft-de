3160fcb78459b844d9d9e2c9f77032d38cdbaacc
Safa-2013 <s.yildiz@lessing-schulen.de>
2026-06-28 15:56:12 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -755,6 +755,6 @@ server.on("upgrade", (req, socket)=>{
 });
 
 server.listen(PORT, ()=>{
-  console.log("Safinicraft.de REAL HOLD + INVENTORY PREVIEW FIX 2.2.3 läuft auf Port " + PORT);
+  console.log("Safinicraft.de ITEM BUG STABLE FIX 2.2.4 läuft auf Port " + PORT);
   console.log("ADMIN_NAME=" + adminName());
 });
